c1743a93ac2a6a1321ac70b3bc62dea923a2c5f8
Panos Matzavinos <matzavin.panos@gmail.com>
2015-01-07 06:32:10 -0500

    Correct the index.js implementation

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-var server = require('./rest_server_mock').create;
+var Promise = require('bluebird')
+    , express = require('express')
+    , fs = require('fs')
 
-server.run();
+    , routesSetup = require('./lib/routes_setup')
+
+    , FILE_ENCODING = require('./constants/contstants').FILE_ENCODING
+
+module.exports = start
+
+function start(serverConfFile) {
+
+    return new Promise(function (resolve, reject) {
+
+        if (!serverConfFile) return resolve()
+
+        fs.readFile(serverConfFile, FILE_ENCODING, read)
+
+        function read(err, data) {
+
+            if (err) return reject(err)
+
+            var serverConf = JSON.parse(data)
+                , routes = serverConf.routes
+                , port = serverConf.port
+                , listener = express()
+                , server = undefined
+
+            routesSetup(listener, routes)
+
+            server = listener.listen(port)
+
+            return resolve(server)
+
+        }
+
+    })
+
+}
+
